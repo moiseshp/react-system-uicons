5d8657debb954f99c4c447a865fdc28c5873fa1f
moises <moises.huaringa>
2022-07-05 13:46:32 -0500

    init project

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-system-uicons'
+import 'react-system-uicons/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="React System UIcons 😄" />
+}
+
+export default App
