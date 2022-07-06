19d3e1e667931633f2d5987aa8d0444e4ae67b79
moises <moises.huaringa>
2022-07-05 19:14:41 -0500

    project init v0.0.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
-import styles from './styles.module.css'
+import ReactDOM from "react-dom/client";
+import { TextInput } from "./lib";
+import Hola from './lib/hello/Hola'
+import World from './lib/hello/World'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const App = () => (
+  <div style={{ width: 640, margin: "15px auto" }}>
+    <h1>Hello React</h1>
+    <Hola />
+    <World />
+    <TextInput label="Email Address" placeholder="name@example.com" />
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
